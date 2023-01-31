02be9dc0521f4b0c92eccd601f018050926c4f21
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-31 10:05:06 +0530

    commentcreate component added

--- client/src/components/CommentCreate.jsx
+++ client/src/components/CommentCreate.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const CommentCreate = () => {
+import axios from 'axios'
+
+const CommentCreate = (props) => {
+
+  const [comment, setComment] = React.useState('');
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:3000/posts/' + props.postId + '/comments', { comment })
+    setComment('')
+  }
   return (
-    <div>CommentCreate</div>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="comment" className='text-base font-medium'>New Comment: </label>
+          <input type="text" id='comment' value={comment} onChange={(e) => { setComment(e.target.value) }} />
+        </div>
+        <button className='text-lg font-semibold bg-blue-500 text-white p-2 rounded-md'>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/ui/PostCard.jsx
+++ client/src/ui/PostCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import CommentCreate from '../components/CommentCreate'
 
 const PostCard = (props) => {
     return (
-        <div key={props.id} className="w-1/3 mb-5 border p-1 ">
+        <div key={props.id} className="w-1/3 mb-5 border-2 p-1 border-collapse">
             <div>
                 <h3 className='font-semibold text-lg'>{props.title}</h3>
             </div>
+            <CommentCreate postId={props.id} />
         </div>
     )
 }
